ff332b10d5c8722453f2bd6fc15dbc473ee6d03d
geysivancampos <geisivancs@gmail.com>
2025-09-17 14:00:46 -0300

    Correção do painel js

--- painel.js
+++ painel.js
@@ -127,7 +127,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const csvString = csvRows.join('\n');
-        const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+        //const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+         const blob = new Blob(['\uFEFF' + csvString], { type: 'text/csv;charset=utf-8;' });
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.setAttribute('href', url);
